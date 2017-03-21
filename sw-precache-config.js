a5ed41b9eedb593cf96d58229de55bb57d42b51c
Chris Cerk <chris@thecerks.com>
2017-03-21 15:33:13 -0400

    Prog. App. handles offline connections

---
+++ sw-precache-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  staticFileGlobs: [
+    'dist/**.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/media/spin.gif',
+    'dist/assets/media/compounds/**/*',
+    'dist/assets/media/logos/*'
+  ],
+  root: 'dist',
+  stripPrefix: 'dist/',
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    urlPattern: /apps\.phar\.umich\.edu[/]study-mc/,
+    handler: 'fastest'
+  }]
+};
+
