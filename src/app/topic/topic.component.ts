4df649f413373c44e49ff67a7789e341bfd0d763
Chris Cerk <chris@thecerks.com>
2017-03-22 14:39:44 -0400

    Re-org of Components into templated html files

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -3,55 +3,18 @@ import { Router } from '@angular/router';
 import { ITopic } from '../shared/interfaces';
 import { MyPercentPipe } from '../shared/pipes/percent.pipe'
 
-@Component({ 
-  //moduleId: module.id,
+
+@Component({
   selector: 'topic',
-  template: `
-  
-  <div class="topic view">
-        <div class="navbar smart-navbar review-print-hide">
-            <ul class="nav navbar-nav">
-                <li class="toolbar-item">
-                    <a routerLink="learn" 
-                       (click)="displayMode = displayModeEnum.Learn" 
-                       [class.active]="displayMode === displayModeEnum.Learn">
-                       <span class="glyphicon glyphicon-education"></span>&nbsp;&nbsp;
-                       <span class ="graphic-title">Learn</span>
-                    </a>
-                </li>
-                <li class="toolbar-item">
-                    <a routerLink="test"
-                       (click)="displayMode = displayModeEnum.Test" 
-                       [class.active]="displayMode === displayModeEnum.Test">
-                        <span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;
-                        <span class ="graphic-title">Test</span>
-                    </a>
-                </li>
-                <li class="toolbar-item">
-                    <a routerLink="review"
-                       (click)="displayMode = displayModeEnum.Review" 
-                       [class.active]="displayMode === displayModeEnum.Review">
-                        <span class="glyphicon glyphicon-repeat"></span>&nbsp;&nbsp;
-                        <span class ="graphic-title">Review</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-            <router-outlet></router-outlet>
-            <br />
-            <br />
-</div>
- 
-  
-  `
+  templateUrl: './topic.component.html',
+  styleUrls: ['./topic.component.css']
 })
+
 export class TopicComponent implements OnInit {
-  
     displayMode: TopicDisplayModeEnum;
     displayModeEnum = TopicDisplayModeEnum;
-
     topic: ITopic;
-  
+
     constructor(private router: Router) { }
 
     ngOnInit() {
@@ -71,11 +34,10 @@ export class TopicComponent implements OnInit {
           break;
       }
     }
-
 }
 
 enum TopicDisplayModeEnum {
-  Learn=0,
-  Test=1,
-  Review=2
+  Learn= 0,
+  Test= 1,
+  Review= 2
 }
--- src/app/topic/topicTest.component.ts
+++ src/app/topic/topicTest.component.ts
@@ -2,77 +2,28 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
 import { ITopic } from '../shared/interfaces';
 import { MyPercentPipe } from '../shared/pipes/percent.pipe'
 import { AlertBoxComponent } from '../shared/alertBox.component';
 import { EndMessageComponent } from '../shared/endMessage.component';
 import { nextPrevAnimation } from '../shared/animations';
-
 import { DataService } from '../core/services/data.service';
 
-type Orientation = ( "void" | "next" | "none" | "previous" );
+
+type Orientation = ( 'void' | 'next' | 'none' | 'previous' );
 
 @Component({
   selector: 'topic-test',
-  animations: [nextPrevAnimation],
-  template: `
-<div *ngIf="topic">
-  <h1><b>Test</b> your Knowledge on {{topic.name}}</h1>
-<div class="content">
-  <end-message *ngIf="moduleIsComplete" [topicName]="topic.name" [incorrectAnswers]="incorrectAnswers"></end-message>
-    <div *ngFor="let testItem of topic.testItems; let i = index" [@NextPrevAnimation]="orientation">
-      <div *ngIf="currentQuestion == i">
-        <br>
-          <h2> {{i/topic.testItems.length | MyPercentPipe }} Complete</h2> 
-        <br>
-        <form #f="ngForm" (ngSubmit)="onSubmit(testItem.answer)" *ngIf="validAnswer" method="post">
-          <p class="test-question">
-            {{testItem.question}}
-            <br>
-            <br>
-            <select class="form-control input-lg" [(ngModel)]="userAnswer" name="userAnswerInput" required>
-              <option  value="" selected="selected" disabled="disabled"></option>
-              <option *ngFor="let option of testItem.options;">{{option}}</option>
-            </select>
-          </p>
-          <div class="action-buttons shaded">
-            <button type="submit" class="btn btn-success">Submit</button>
-          </div>
-        </form>
-
-        <div class="action-buttons shaded">
-          <button (click)="retryQuestion()" class="btn btn-danger" *ngIf="!validAnswer">Retry</button>
-        </div>
-
-         <div class="alert alert-danger" role="alert" *ngIf="!validAnswer">
-            <a href="#" class="alert-link">Incorrect Answer!</a>
-         </div>
-
-      </div>
-    </div>
-
-    <div *ngIf="!topic.testItems.length">
-        <div class="alert alert-info review-alert" role="alert">
-          <a href="#" class="alert-link"> There are currently no Test items for {{topic.name}}.</a>
-        </div>
-    </div>
-
-</div>
-<div *ngIf="!topic" class="row">
-  No topic found
-</div>
-</div>
-                
-`
+  templateUrl: './topicTest.component.html',
+  styleUrls: ['./topicTest.component.css'],
+  animations: [nextPrevAnimation]
 })
+
 export class TopicTestComponent implements OnInit {
 
   topic: ITopic;
   private sub: Subscription;
-
   router: Router;
-
   userAnswer: string;
   correctAnswer: string;
   currentQuestion: number;
@@ -82,15 +33,20 @@ export class TopicTestComponent implements OnInit {
 
   public orientation: Orientation;
 
-  constructor(private route: ActivatedRoute, router: Router, private dataService: DataService, private changeDetectorRef: ChangeDetectorRef) {
+  constructor(
+    private route: ActivatedRoute,
+    router: Router,
+    private dataService: DataService,
+    private changeDetectorRef: ChangeDetectorRef) {
+
       this.router = router;
       this.validAnswer = true;
-      this.userAnswer = "Current Question Not Answered Yet";
+      this.userAnswer = 'Current Question Not Answered Yet';
       this.currentQuestion = 0;
       this.incorrectAnswers = 0;
       this.moduleIsComplete = false;
       this.changeDetectorRef = changeDetectorRef;
-   } 
+   }
 
   ngOnInit() {
       this.sub = this.route.parent.params.subscribe(params => {
@@ -99,7 +55,7 @@ export class TopicTestComponent implements OnInit {
             .subscribe((topic: ITopic) => this.topic = topic);
       });
 
-      this.orientation = "void";
+      this.orientation = 'void';
   }
 
   ngOnDestroy() {
@@ -117,17 +73,17 @@ export class TopicTestComponent implements OnInit {
   }
 
   nextQuestion() : void {
-    this.orientation = "next";
+    this.orientation = 'next';
     this.changeDetectorRef.detectChanges();
 
-    this.userAnswer = "Current Question Not Answered Yet";
+    this.userAnswer = 'Current Question Not Answered Yet';
     this.currentQuestion++;
 
     if(this.topic.testItems.length == this.currentQuestion)
     {
       this.moduleIsComplete = true;
     }
-    this.orientation = "void";
+    this.orientation = 'void';
   }
 
   wrongAnswer(){
@@ -135,9 +91,8 @@ export class TopicTestComponent implements OnInit {
   }
 
   retryQuestion(){
-    this.userAnswer = "Current Question Not Answered Yet";
+    this.userAnswer = 'Current Question Not Answered Yet';
     this.validAnswer = true;
     this.incorrectAnswers++;
   }
-
 }
