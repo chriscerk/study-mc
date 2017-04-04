23a7bbf7486d2ef1b750512c544ac610ab333e35
Chris Cerk <chris@thecerks.com>
2017-04-04 13:06:42 -0400

    GetCourse by id in Course service

--- src/app/core/services/course/course.service.ts
+++ src/app/core/services/course/course.service.ts
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CourseService {
+  courses: ICourse[];
   courseDataUrl = 'assets/data/courses.json';
   errorMessage = 'Course Data could not be retrieved.' +
    '\n 1. Verify ' + this.courseDataUrl + ' exists. \n 2. JSON Lint the courses.json file.';
@@ -17,16 +18,52 @@ export class CourseService {
   constructor(private http: Http, private errorHandler: ErrorHandlerService) { }
 
   getCourses(): Observable<ICourse[]> {
-        return this.http
-          .get(this.courseDataUrl)
-          .map((res: Response) => {
-              let courses = res.json();
-              return courses;
-          })
-          .catch(this.handleCourseError.bind(this));
+    if(!this.courses) {
+      return this.http
+      .get(this.courseDataUrl)
+      .map((res: Response) => {
+          let courses = res.json();
+          return courses;
+      })
+      .catch(this.handleCourseError.bind(this));
+    } else {
+        return this.createObservable(this.courses);
     }
+  }
 
-    handleCourseError(error: any) {
-      return this.errorHandler.handleWithMessage(error, this.errorMessage);
+  getCourse(id: number): Observable<ICourse> {
+    if (this.courses) {
+        return this.findCourseObservable(id);
+    } else {
+        return Observable.create((observer: Observer<ICourse>) => {
+                this.getCourses().subscribe((courses: ICourse[]) => {
+                    this.courses = courses;
+                    const course = this.filterCourses(id);
+                    observer.next(course);
+                    observer.complete();
+            });
+        })
+        .catch(this.handleCourseError.bind(this));
     }
+  }
+
+  private handleCourseError(error: any) {
+    return this.errorHandler.handleServiceWithMessage(error, this.errorMessage);
+  }
+
+  private filterCourses(id: number) : ICourse {
+    const cs = this.courses.filter((c) => c.id === id);
+    return (cs.length) ? cs[0] : null;
+  }
+
+  private findCourseObservable(id: number) : Observable<ICourse> {
+      return this.createObservable(this.filterCourses(id));
+  }
+
+  private createObservable(data: any): Observable<any> {
+    return Observable.create((observer: Observer<any>) => {
+        observer.next(data);
+        observer.complete();
+    });
+  }
 }
