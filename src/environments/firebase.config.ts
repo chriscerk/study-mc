16b18984749b238457494d7990b7e85702787bd1
Cerk <chriscerk@users.noreply.github.com>
2017-04-12 08:00:54 -0400

    Delete firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCIyjUzSQHJHG_1H3qdetFpiU80ugtzhpQ",
-    authDomain: "study-mc.firebaseapp.com",
-    databaseURL: "https://study-mc.firebaseio.com",
-    projectId: "study-mc",
-    storageBucket: "study-mc.appspot.com",
-    messagingSenderId: "274469097954"
-  };
